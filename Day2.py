574cdfd8b890de62069fc1632440338b638d27c0
punithdalli789 <98897816+punithdalli789@users.noreply.github.com>
2022-02-02 21:23:36 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,6 @@
+a="hello coders"
+inp=input("enter a letter: ")
+if inp==a[0].lower() or inp==a[1].lower() or inp==a[4].lower() or inp==a[6].lower() or inp==a[8].lower():
+    print("you get the prize")
+else:
+    print("you are guessing is wrong")
